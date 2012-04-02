a6e54e2e95a6da319b2fcf8b954d3ca48844409f
cbenard <chris@chrisbenard.net>
2012-04-01 19:34:11 -0500

    Updated sample secrets. Created settings provider to read it.

---
+++ SO Dailymotion Upload/SettingsProvider.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace SO_Dailymotion_Upload
+{
+    public static class SettingsProvider
+    {
+        public static string Key
+        {
+            get
+            {
+                return GetXmlElementValue("key");
+            }
+        }
+
+        public static string Secret
+        {
+            get
+            {
+                return GetXmlElementValue("secret");
+            }
+        }
+
+        private static string GetXmlElementValue(string elementName)
+        {
+            return XDocument.Load("secrets.xml").Element("root").Element(elementName).Value;
+        }
+    }
+}
